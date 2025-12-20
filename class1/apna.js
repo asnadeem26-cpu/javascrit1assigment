4421cf4e82b6322670f9a832bb1105cf41a4fbff
asnadeem26-cpu <asnadeem26@gmail.com>
2025-12-20 06:25:44 -1000

    javascript

---
+++ class1/apna.js
@@ -0,0 +1,8 @@
+alert("are you ?")
+var javascript = "hello world"
+console.log(javascript)
+console.log(javascript)
+javascript = "ayesha"
+console.log(javascript);
+var assigment
+document.write("my first assigment java script")
