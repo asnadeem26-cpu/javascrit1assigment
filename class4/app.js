81d96b5187f2f2cfe9fc60bde605aeb657f34fbb
asnadeem26-cpu <asnadeem26@gmail.com>
2025-12-25 21:51:01 -1000

    message

---
+++ class4/app.js
@@ -0,0 +1,9 @@
+var firstname =prompt("enter your name","ayesha");
+var lastname =prompt("enter your lastname","nadeem");
+var age =prompt("age" ,"20");
+var personald = prompt("contact");
+
+var fullname = "ms" +firstname +" " +lastname+"!"
+console.log(fullname)
+var wellcome = "wellocome to the group," +firstname+" " +lastname 
+document.write("<h1>" +firstname+ lastname+"</h1>" +"<p>"+"On behalf of everyone, welcome! We're looking forward to achieving great things together."+"We're delighted to have you join us. Your skills are a perfect fit, and we can't wait to see what you'll accomplish."+"Welcome aboard! Feel free to reach out with any questions as you settle in." +"</p>")
