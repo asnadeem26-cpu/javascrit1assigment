0f59d7b27e47bfd344b88c635e16e10035e4fa2c
asnadeem26-cpu <asnadeem26@gmail.com>
2025-12-25 21:06:40 -1000

    java script

---
+++ class3/app.js
@@ -0,0 +1,31 @@
+var num = 11
+num--
+console.log(num--);//10
+console.log(num--);//9
+num++
+console.log(num++);//9
+console.log(num++);//10
+
+var a=5
+var b=10
+var c= a+b
+console.log(c);//15
+
+var a=4
+var b= 3
+var c= a*b
+console.log(c);//12
+
+var num=13
+num--
+console.log(num--) //12
+num++
+num++
+console.log(num++) //13
+console.log(--num)//13
+
+var a = "ayesha"
+var num = 22
+var c= a+num
+console.log(c)
+//ayesha22
