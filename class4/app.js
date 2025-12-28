fa0db42a2abe4f63b0027000dd64d3f7fb367dcf
asnadeem26-cpu <asnadeem26@gmail.com>
2025-12-28 02:14:17 -1000

    Deleted folder_name

--- class4/app.js
+++ class4/app.js
@@ -1,9 +1,68 @@
-var firstname =prompt("enter your name","ayesha");
-var lastname =prompt("enter your lastname","nadeem");
-var age =prompt("age" ,"20");
-var personald = prompt("contact");
+var city =prompt("enter your city name" ,"karachi");
 
-var fullname = "ms" +firstname +" " +lastname+"!"
-console.log(fullname)
-var wellcome = "wellocome to the group," +firstname+" " +lastname 
-document.write("<h1>" +firstname+ lastname+"</h1>" +"<p>"+"On behalf of everyone, welcome! We're looking forward to achieving great things together."+"We're delighted to have you join us. Your skills are a perfect fit, and we can't wait to see what you'll accomplish."+"Welcome aboard! Feel free to reach out with any questions as you settle in." +"</p>")
+if(city .tolowecase()==="karachi"){
+    document.write("wellcome to city of lights");
+} else{
+    document.write("wellcome to "+ city)
+}
+
+//GENDER
+var gender =prompt("enter your gender( male/ female)");
+if(gender.tolowecase()=== "MALE"){
+    document.write("GOOD MORNING SIR");
+
+}else if(gender.tolowercase()==="female"){
+    document.write("GOOD MORNING MAM")
+}
+//TRAFIC
+var signalcolor =("enter traffic color ");
+if(signalcolor.tolowecase()==="red"){
+    document.write("must stop")
+
+}else if(signalcolor.toLowerCase()==="yellow"){
+    document.write("ready to move")
+}else if(signalcolor.toLowerCase()==="green"){
+    document.write("move now")
+}
+
+//4
+var  fuel = prompt("enter remaing fuel");
+fuel= Number(fuel);
+if(fuel< 0.25){
+    document.write("please refill the fuel")
+}
+
+//5
+var a=5
+ if(++a===6)
+var b=82
+if(b++===83){
+    alert("given condition variable is true")
+
+}
+//6
+var markobtain=252;
+var totalmarks=300
+var percentage=(markobtain/totalmarks)*100
+var grade;
+var remarks;
+
+if(percentage >=80){
+    grade="A-one";
+    remarks="excellent";
+
+}else if(percentage >=70){
+ grade="A";
+ remarks="good"
+}else if (percentage >=  60 ){
+    grade="B";
+    remarks="improve your self"
+
+}
+    console.log("maeksheet");
+    console.log("totalmarks"+totalmarks);
+    console.log("marks obtained")
+    console.log("percentage")
+    console.log("grade")
+    console.log("remarks")
+   
--- class5/app.js
+++ class5/app.js
@@ -1,8 +1,68 @@
-var  city = prompt("enter your ", "karachi")
-var gender = prompt("enter your gender" , "female")
+var city =prompt("enter your city name" ,"karachi");
 
-document.write("<h2>" + city +"</h2>"+ "<p>" + wellcome +to+ city + OF +light+"</p>")
-// document.write (female("welllocome to mam")){
-//     console.log("wellocome to mam");
-// }
-// else{console .log("wellocome to sir");}
+if(city .tolowecase()==="karachi"){
+    document.write("wellcome to city of lights");
+} else{
+    document.write("wellcome to "+ city)
+}
+
+//GENDER
+var gender =prompt("enter your gender( male/ female)");
+if(gender.tolowecase()=== "MALE"){
+    document.write("GOOD MORNING SIR");
+
+}else if(gender.tolowercase()==="female"){
+    document.write("GOOD MORNING MAM")
+}
+//TRAFIC
+var signalcolor =("enter traffic color ");
+if(signalcolor.tolowecase()==="red"){
+    document.write("must stop")
+
+}else if(signalcolor.toLowerCase()==="yellow"){
+    document.write("ready to move")
+}else if(signalcolor.toLowerCase()==="green"){
+    document.write("move now")
+}
+
+//4
+var  fuel = prompt("enter remaing fuel");
+fuel= Number(fuel);
+if(fuel< 0.25){
+    document.write("please refill the fuel")
+}
+
+//5
+var a=5
+ if(++a===6)
+var b=82
+if(b++===83){
+    alert("given condition variable is true")
+
+}
+//6
+var markobtain=252;
+var totalmarks=300
+var percentage=(markobtain/totalmarks)*100
+var grade;
+var remarks;
+
+if(percentage >=80){
+    grade="A-one";
+    remarks="excellent";
+
+}else if(percentage >=70){
+ grade="A";
+ remarks="good"
+}else if (percentage >=  60 ){
+    grade="B";
+    remarks="improve your self"
+
+}
+    console.log("maeksheet");
+    console.log("totalmarks"+totalmarks);
+    console.log("marks obtained")
+    console.log("percentage")
+    console.log("grade")
+    console.log("remarks")
+   
