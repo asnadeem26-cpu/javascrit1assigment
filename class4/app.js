ccd63cf8176215448fd2901f413c154ed8a66350
asnadeem26-cpu <asnadeem26@gmail.com>
2025-12-28 06:59:43 -1000

    java

--- class4/app.js
+++ class4/app.js
@@ -1,68 +1,27 @@
-var city =prompt("enter your city name" ,"karachi");
+var num=10
+console.log(++num)//11
+num++
 
-if(city .tolowecase()==="karachi"){
-    document.write("wellcome to city of lights");
-} else{
-    document.write("wellcome to "+ city)
-}
+console.log(num++)//12
 
-//GENDER
-var gender =prompt("enter your gender( male/ female)");
-if(gender.tolowecase()=== "MALE"){
-    document.write("GOOD MORNING SIR");
+var num=12
+num--
+console.log(num--)//11
+console.log(num--)//10
 
-}else if(gender.tolowercase()==="female"){
-    document.write("GOOD MORNING MAM")
-}
-//TRAFIC
-var signalcolor =("enter traffic color ");
-if(signalcolor.tolowecase()==="red"){
-    document.write("must stop")
-
-}else if(signalcolor.toLowerCase()==="yellow"){
-    document.write("ready to move")
-}else if(signalcolor.toLowerCase()==="green"){
-    document.write("move now")
-}
-
-//4
-var  fuel = prompt("enter remaing fuel");
-fuel= Number(fuel);
-if(fuel< 0.25){
-    document.write("please refill the fuel")
-}
-
-//5
 var a=5
- if(++a===6)
-var b=82
-if(b++===83){
-    alert("given condition variable is true")
+var b=4
+console.log(a*b)
+console.log(5*4)//20
 
-}
-//6
-var markobtain=252;
-var totalmarks=300
-var percentage=(markobtain/totalmarks)*100
-var grade;
-var remarks;
+var fname=prompt("Enter your name")
+var lname = prompt("enter your last name")
+var contact= prompt("enter your contact")
+var age =prompt("enter your age")
+var fullnamen= "ms"+fname+" "+lname+"!"
+console.log(fullnamen )
 
-if(percentage >=80){
-    grade="A-one";
-    remarks="excellent";
 
-}else if(percentage >=70){
- grade="A";
- remarks="good"
-}else if (percentage >=  60 ){
-    grade="B";
-    remarks="improve your self"
+var wellcome = "wellcome our company"+So +glad +your+ "here Lets get started"+"Welcome aboard! Looking forward to working with you."
 
-}
-    console.log("maeksheet");
-    console.log("totalmarks"+totalmarks);
-    console.log("marks obtained")
-    console.log("percentage")
-    console.log("grade")
-    console.log("remarks")
-   
+document.write("<h1>"+fname+lname+"</h1>"+"<p>"+wellcome+"</p>")
