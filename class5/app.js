b6ea574d4fb50a2ae2a01b6040cf692d8e64a545
asnadeem26-cpu <asnadeem26@gmail.com>
2025-12-28 01:58:33 -1000

    java

---
+++ class5/app.js
@@ -0,0 +1,8 @@
+var  city = prompt("enter your ", "karachi")
+var gender = prompt("enter your gender" , "female")
+
+document.write("<h2>" + city +"</h2>"+ "<p>" + wellcome +to+ city + OF +light+"</p>")
+// document.write (female("welllocome to mam")){
+//     console.log("wellocome to mam");
+// }
+// else{console .log("wellocome to sir");}
