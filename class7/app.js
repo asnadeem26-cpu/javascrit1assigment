908cb344eae040cf9c83f9468856481bc9a6b244
asnadeem26-cpu <asnadeem26@gmail.com>
2026-01-01 21:53:06 -1000

    javascript

---
+++ class7/app.js
@@ -0,0 +1,56 @@
+
+
+
+
+
+// //fasly vale 
+// console.log(!!NaN)
+// console.log(!!0)
+// console.log(!!false)
+// console.log(!!false)
+// console.log(!!"")
+// console.log(!!undefined)
+// console.log(!!null)
+// console.log(!!Boolean)
+
+//
+// var html=+prompt("can you use html")
+// var bootstrap=+prompt("can you use bootsrap")
+// var css=+prompt("canyou use css")
+// var java=+prompt
+// ("can you use java")
+// if((html=="yes"&&bootstrap=="yes")) {if(css==="yes"&&java==="yes")
+//     console.log("you are eligable")
+   
+// }else{
+//     console.log("you are eligablr")
+// }
+
+// // else{
+
+// //     console.log("false")
+// // }
+// //     console.log("false")
+// // exceise
+// //q1
+
+var studentname=[];
+var studentname=new Array()
+var fruit = ("apple","banana","orange")
+var number=[20,30,40]
+var fasly=[false,true,false]
+var  mixedArray=["ayesha",25,true,null]
+
+var qulaification=["ssc","hsc","bcs","bs","bcom","ms","m.phil","phd"]
+
+
+document.write("<h2>QUALIFICATION</h2>")
+document.write("<h3>"+qulaification+"</h3>")
+
+//
+var student= ["ayesha ","fizza","saba"]
+
+var score =[320 , 230 ,480]
+var totalmark=500;
+document.write("score  of"+student[0]+ "is"+score[0]+"percentage"+(score[1]/totalmark*100)+"%<br>")
+document.write("score"+student[0]+"is"+ score[0]+"percentage"+(score[2]/totalmark*100)+"%<br")
